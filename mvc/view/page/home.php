fdfe9b31ebfdc6b100bb045f2a6d29354e57ef06
Hadi14 <asusn43@yahoo.com>
2023-08-21 08:24:54 +0330

    حل نهایی مشکل حذف رکورد با Ajax

--- mvc/view/page/home.php
+++ mvc/view/page/home.php
@@ -20,11 +20,8 @@
             <td><?= $row['noteTitle'] ?></td>
             <td><?= $row['noteText'] ?></td>
             <td><?= $row['noteTime'] ?></td>
-
             <td><span class="link" onclick="editNote(this,<?= $row['noteID'] ?>)"> <i class="bi bi-pencil-square"></i></span></td>
-
             <td><span class="link" onclick="deleteNote(this,<?= $row['noteID'] ?>)"> <i class="bi bi-calendar-x"></i></span></td>
-
         </tr>
     <? } ?>
 </table><br>
@@ -38,11 +35,9 @@
     function deleteNote(sender, noteId) {
         sender = $(sender);
         var parent = sender.parentsUntil('tr').parent();
-
         $.ajax('/notes3-uncox-withMVCandAJAX/notes/remove/' + noteId, {
             type: 'post',
             dataType: "JSON",
-
             success: function(data) {
                 console.log("SUCCESS Ok");
                 parent.remove();
