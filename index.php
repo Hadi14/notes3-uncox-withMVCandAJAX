84052b7de18619f2bec97822371b7c3cecf4b76c
Hadi14 <asusn43@yahoo.com>
2023-08-21 09:17:54 +0330

    حل نهایی مشکل حذف رکورد با Ajax-2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $parts = explode('/', $uri);
 $controller = $parts[1];
 $method = $parts[2];
 
-
+//(مهم مهم)  اگر این اکوها را بزاریم ، بعد ، ایجکس حذف رکورد درست کار نمیکند یعنی هیچ پرینت و تگ اچ تی ام ال نباید در صفحه ایندکس گذاشته شود
 // echo "<hr> controller: " . $controller;
 // echo "<br> method: " . $method . "<br>";
 
